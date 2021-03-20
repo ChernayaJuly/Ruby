e33f183503e425eb6f1da58a88ffc1412fe3a0dc
ChernayaJuly <c.julia@rambler.ru>
2021-03-20 09:50:16 +0300

    lab1 z7 method1
    еще не работает

--- lab1_z7.rb
+++ lab1_z7.rb
@@ -3,29 +3,24 @@ numb = ARGV[0]
 numb = numb.to_i
 
 def prost numb
-	if(numb==1)
-		return false
 	for i in 2..Math.sqrt(numb)
-		if(numb%==0)
+		if(numb%i==0)
 			return false
+		else
+			return true
 		end
 	end
 end
 
 def sum_prost numb
-	s=[]
-	s[1]=0
-	for k in 2..numb
-		s[k]==1
-	end
-	for k in 2..Math.sqrt(numb)
-		if (s[k]==1)
-			for j in k*k..N
-				s[j]==0
-			end
+	sum=0
+	for k in 2..numb/2
+		if(numb%k==0 && prost(k)==true)
+			sum+=k
 		end
 	end
-	return s
+	sum
 end
 
+puts sum_prost numb
 
