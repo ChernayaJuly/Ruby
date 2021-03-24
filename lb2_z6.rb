2d338b94447003742a2612817e1095e2363116ce
ChernayaJuly <c.julia@rambler.ru>
2021-03-24 17:46:06 +0300

    lab2 z6_1 norm

--- lb2_z6.rb
+++ lb2_z6.rb
@@ -1,36 +1,25 @@
 #v-1
-puts "введите кол-во эл и числа"
-arr=Array.new( ( gets.to_i ) ){ |i| gets.to_i } 
 
 #z1
-def max_el arr
-	return arr.max
-end
-
- def m_i arr 
- 	m = arr.max
- 	max_ind = arr.index(m)
- 	return max_ind
- end
-
 def z_1 arr
-	m = max_el arr
-	max_ind = m_i arr
-	s = 0
-	for i in 0...arr.size
-    	if(i>max_ind)
-      		s+=1
-		end
+	m = arr.max
+	max_ind = arr.index(m)
+	k = 0
+	for i in (max_ind+1)..(arr.length)-1
+		k+=1
 	end
-		if (m==arr.last)
-			puts "Max = last element"
-		else
-		s
-		end
+	if (m==arr.last)
+		puts "Max = last element"
+	else
+		k
+	end
 end
-puts z_1 arr
+
 
 #z13
 #z25
 #z37
 #z49
+puts "введите кол-во эл и числа"
+arr=Array.new( ( gets.to_i ) ){ |i| gets.to_i } 
+puts z_1 arr
