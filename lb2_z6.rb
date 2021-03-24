439d4a27e29a68911195bdf8aff377541ac23efb
ChernayaJuly <c.julia@rambler.ru>
2021-03-24 21:07:16 +0300

    lab2 z6_5

--- lb2_z6.rb
+++ lb2_z6.rb
@@ -63,9 +63,32 @@ def z_37 arr
 end
 
 #z49
+def prost numb
+	for i in 2..Math.sqrt(numb)
+		if(numb%i==0)
+			return false
+		end
+	end
+	return true
+end
+
+def z_49 arr
+	arr1 = []
+	for i in 0..(arr.length)-1
+		for j in 2..arr[i]/2
+			if arr[i]%j==0 && (prost(j)==true)
+				arr1.push(j)
+			end
+		end
+	end
+	puts print arr1.uniq	
+end
+
+
 puts "введите кол-во эл и числа"
 arr=Array.new( ( gets.to_i ) ){ |i| gets.to_i } 
 #puts z_1 arr
 #z_13 arr
 #z_25 arr
-z_37 arr
+#z_37 arr
+z_49 arr
