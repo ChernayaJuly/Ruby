dc60c3b36eb9eb9be3f7daf9d0ebcbf75939fe5b
ChernayaJuly <c.julia@rambler.ru>
2021-03-24 15:57:13 +0300

    lab2 z6_2

--- lb2_z6.rb
+++ lb2_z6.rb
@@ -3,26 +3,32 @@ puts "введите кол-во эл и числа"
 arr=Array.new( ( gets.to_i ) ){ |i| gets.to_i } 
 
 #z1
-def z_1
-	arr = read 'file.txt'
-	m = arr.max
-	max_ind = arr.index(max)
+def max_el arr
+	return arr.max
+end
+
+ def m_i arr 
+ 	m = arr.max
+ 	max_ind = arr.index(m)
+ 	return max_ind
+ end
+
+def z_1 arr
+	m = max_el arr
+	max_ind = m_i arr
 	s = 0
-	arr.each do |i| 
-		if ((arr.index(i) > max_ind) && arr[i]!=m)
-			s +=i
-		elsif ((arr.index(i) > max_ind) && arr[i]==m)
-			s = 0 
-			max_ind = arr.index(i)
+	for i in 0...arr.size
+    	if(i>max_ind)
+      		s+=1
 		end
+	end
 		if (m==arr.last)
 			puts "Max = last element"
 		else
-			s
-		end	
-	end
+		s
+		end
 end
-puts z_1
+puts z_1 arr
 
 #z13
 #z25
