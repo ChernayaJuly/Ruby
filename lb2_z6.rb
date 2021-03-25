7ab2a618bbfd33cad50f79870cb2e2d1780c966b
ChernayaJuly <c.julia@rambler.ru>
2021-03-25 03:36:17 +0300

    lab2 z7 dodelat

--- lb2_z6.rb
+++ lb2_z6.rb
@@ -2,32 +2,24 @@
 
 #z1
 def z_1 arr
-	input
-	m = arr.max
-	max_ind = arr.index(m)
-	k = 0
-	for i in (max_ind+1)..(arr.length)-1
-		k+=1
-	end
+	max_ind = arr.index(arr.max)
 	if (m==arr.last)
 		puts "Max = last element"
 	else
-		puts k
+		puts arr.reverse.index(arr.max)
 	end
 end
 
 
 #z13
 def z_13 arr
-	input
 	m = arr.min
 	min_ind = arr.index(m)
 	if (m==arr.first)
 		puts "Min = first element"
 	else
-		for i in 0..min_ind-1
+		arr.each do |i|
 		arr.push(arr.shift)
-		end
 	end
 	puts print arr
 end
@@ -41,24 +33,21 @@ def z_25 arr
 	puts "Введите b: "
 	b = gets.to_i
 	arr1 = []
-	for i in 0..(arr.length)-1
-		if (arr[i]>a && arr[i]<b)
-			arr1.push(arr[i])
+	arr.each do |i|
+		if i>a && i<b
+			arr1.push(i)
 		end
-	end
 	puts arr1.max
 end
 
 #z37
 def z_37 arr
 	arr1=[]
-	k = 0
-	for i in 1..(arr.length)-1
+	k = arr.select {|i| arr.index(i).to_s<arr.index(i-1).to_s}.length
+	(1..(arr.length)-1).each do |i|
 		if arr[i]<arr[i-1]
 			arr1.push(i)
-			k+=1
 		end
-	end
 	puts "indexes:" 
 	puts print arr1
 	puts "kolvo = ", k
@@ -66,23 +55,20 @@ end
 
 #z49
 def prost numb
-	for i in 2..Math.sqrt(numb)
+	(2..Math.sqrt(numb)).each do |i|
 		if(numb%i==0)
 			return false
 		end
-	end
 	return true
 end
 
 def z_49 arr
 	arr1 = []
-	for i in 0..(arr.length)-1
-		for j in 2..arr[i]/2
+	(0..(arr.length)-1).each do |i|
+		(2..arr[i]/2).each do |j|
 			if arr[i]%j==0 && (prost(j)==true)
 				arr1.push(j)
 			end
-		end
-	end
 	puts print arr1.uniq	
 end
 
@@ -93,18 +79,18 @@ def main
 	puts "введите кол-во эл и числа"
 	arr=Array.new( ( gets.to_i ) ){ |i| gets.to_i } 
 	case n
-	when 1
-		z_1 arr
-	when 2
-		z_13 arr
-	when 3
-		z_25 arr
-	when 4
-		z_37 arr
-	when 5
-		z_49 arr
-	else
-		puts "Такой задачи нет!"
+		when 1
+			z_1 arr
+		when 2
+			z_13 arr
+		when 3
+			z_25 arr
+		when 4
+			z_37 arr
+		when 5
+			z_49 arr
+		else
+			puts "Такой задачи нет!"
 	end
 end
 
