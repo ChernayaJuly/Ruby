7c40f9e7f06a7518bf38df63222dc8a421c0bf8d
ChernayaJuly <c.julia@rambler.ru>
2021-03-26 12:45:25 +0300

    lb2 z9 all

--- lb2_z8.rb
+++ lb2_z8.rb
@@ -1,36 +1,47 @@
 #v-1
 
-def zz_8 s
-	puts s.split.map{ |i| i.to_i }.max
-end
+	def zz_8
+		puts s = "2 17 9 5 4 88 2 1"
+		puts s.split.map{ |i| i.to_i }.max
+	end
 
 
-#z1
-def z_1 s
-	puts s.scan(/[А-я]/).size
-end
+	#z1
+	def z_1
+		puts s = "dgsdАВГvnjvnjзвйд123"
+		puts s.scan(/[А-я]/).size
+	end
 
 
-#z9 s = "МАДАМ abba"
-def z_9 s
-	a = s.scan /[a-zа-я]/
-	puts a == a.reverse
-end
+	#z9 
+	def z_9
+		puts s = "МАДАМ abba"
+		a = s.scan /[a-zа-я]/
+		puts a == a.reverse
+	end
 
 
-#z18
-def z_18
-	s = "09.03.2005 я гуляля с Машей, а 15.Августа.2007 я была на море"
-	puts s.scan(/([0-2]\d|3[0-1]).(0[1-9]|1[0-2]|Января|Февраля|Марта|Апреля|Мая|Июня|Июля|Августа|Сентября|Октября|Ноября|Декабря).(\d{4})/).map{|e| e.join('.')}.join(", ")
-end
+	#z18
+	def z_18
+		puts s = "09.03.2005 я гуляля с Машей, а 15.Августа.2007 я была на море"
+		puts s.scan(/([0-2]\d|3[0-1]).(0[1-9]|1[0-2]|Января|Февраля|Марта|Апреля|Мая|Июня|Июля|Августа|Сентября|Октября|Ноября|Декабря).(\d{4})/).map{|e| e.join('.')}.join(", ")
+	end
 
 
-
-
-
-s = "09.03.2005 я гуляля с Машей, а 15.Августа.2007 я была на море"
-#s = "15.Августа.2009 светило солнце, а 16.03.2009"
-#zz_8 s
-#z_1 s
-#z_9 s
-z_18 s
+	def main
+		puts "Какую задачу хотите решить?"
+		n = gets.to_i 
+		case n
+		when 1
+			zz_8
+		when 2
+			z_1
+		when 3
+			z_9
+		when 4
+			z_18
+		else
+			puts "Такой задачи нет!"
+		end
+	end
+main
