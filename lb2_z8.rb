324753d710c4858ad6f98e5f293e06ef1478329c
ChernayaJuly <c.julia@rambler.ru>
2021-03-26 12:10:42 +0300

    lab2 z9_2

--- lb2_z8.rb
+++ lb2_z8.rb
@@ -1,12 +1,23 @@
 #v-1
 
+def zz_8 s
+	puts s.split.map{ |i| i.to_i }.max
+end
+
+
 #z1
 def z_1 s
 	puts s.scan(/[А-я]/).size
 end
 
 
-#z9
+#z9 s = "МАДАМ abba"
+def z_9 s
+	a = s.scan /[a-zа-я]/
+	puts a == a.reverse
+end
+
+
 #z18
 
 
@@ -24,16 +35,7 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-s = "FnjkfААяоgfаилrrDDЬЬП"
-#puts s.split.map{ |i| i.to_i }.max
-z_1 s
+s = "МАДАМ abba"
+#zz_8 s
+#z_1 s
+z_9 s
