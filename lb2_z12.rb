280b202d8d6a783bcc5ece7fb74776851a1b7583
ChernayaJuly <c.julia@rambler.ru>
2021-03-26 16:11:27 +0300

    lab2 z13

--- lb2_z12.rb
+++ lb2_z12.rb
@@ -2,6 +2,6 @@
 #z_12
 def read 
 	s = File.open('file_str.txt'){ |file| file.read}
-	puts print (s.split("\n").sort_by{|i| i.length}).join("\n")
+	puts print (s.split("\n").sort_by{|i| i.split.length}).join("\n")
 end
 read
