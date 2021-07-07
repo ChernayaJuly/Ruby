bec68ffb3e8cce80d07bc518b168d27f1c4e4670
ChernayaJuly <c.julia@rambler.ru>
2021-07-07 12:13:25 +0300

    1

--- TestDB.rb
+++ TestDB.rb
@@ -17,6 +17,12 @@ def self.instance
   @@instance ||= new
 end
 
+def self.get_instance
+  if @@instance == nil
+    @@instance - DB.new
+  end
+  
+end
 def db_mysql_con
   Mysql2::Client.new(host: '127.0.0.1', username: 'yulia', password: 'Yulia2000', database: 'Stuff')
 end
